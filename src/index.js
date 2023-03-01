73dcae3e4de14700a52e3bb8956f16e5c975bd5b
SebastianZasadni <emblelele@gmail.com>
2023-03-01 10:42:17 +0100

    feat task-10

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 import './css/styles.css';
 
 const DEBOUNCE_DELAY = 300;
+const inputBtn = document.querySelector("#search-box");
+const countryList = document.querySelector('.country-list');
+const countryInfo = document.querySelector('.country-info');
+
+inputBtn.addEventListener("input", 
+_.debounce(() => {
+ checkCountries();
+}, DEBOUNCE_DELAY));
+
+const checkCountries = () => {
+  const fetchCountries = fetch('https://restcountries.com/v3.1/all');
+  fetchCountries.then((response) => {
+  const countriesData = response.json();
+  return countriesData;
+}).then((response) => {
+    response.map(country=>{
+    if(country.name.common === inputBtn.value){
+      const markup = `<li>${country.name.official}</li>
+      <li>${country.capital}</li>
+      <li>${country.population}</li>
+      <li>${country.flags.svg}</li>
+      <li>${Object.values(country.languages)}</li>`;
+            return countryList.innerHTML = markup;
+        }
+        
+     })
+  })
+};
